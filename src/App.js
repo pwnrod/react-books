66d92bf0653597b369236c6340bc1969718b994a
Caylin James <cjames@leapagency.com>
2023-05-17 12:35:55 -0400

    add basic functionality for displaying the list of books

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
     const [books, setBooks] = useState([]);
@@ -16,7 +17,8 @@ function App() {
     };
 
     return (
-        <div>
+        <div className="app">
+            <BookList books={books} />
             <BookCreate onCreate={handleCreateBook} />
         </div>
     );
