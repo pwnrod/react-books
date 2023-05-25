308b4ff8468a2732905baa70b1631971dc767f1c
Caylin James <cjames@leapagency.com>
2023-05-25 19:13:37 -0400

    add the useCallback hook so that our fetchBooks function is bug free

--- src/context/books.js
+++ src/context/books.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useCallback } from 'react';
 import axios from 'axios';
 
 const BooksContext = createContext();
@@ -6,11 +6,11 @@ const BooksContext = createContext();
 const Provider = ({ children }) => {
     const [books, setBooks] = useState([]);
 
-    const fetchBooks = async () => {
+    const fetchBooks = useCallback(async () => {
         const response = await axios.get('http://localhost:3001/books');
 
         setBooks(response.data);
-    };
+    }, []);
 
     const deleteBookById = async (id) => {
         await axios.delete(`http://localhost:3001/books/${id}`);
@@ -23,8 +23,7 @@ const Provider = ({ children }) => {
     };
 
     const editBookById = async (id, newTitle) => {
-        const response = await axios.put(`http://localhost:3001/books/${id}`,
-            { title: newTitle });
+        const response = await axios.put(`http://localhost:3001/books/${id}`, { title: newTitle });
 
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
@@ -48,15 +47,11 @@ const Provider = ({ children }) => {
         deleteBookById,
         editBookById,
         createBook,
-        fetchBooks
-    }
+        fetchBooks,
+    };
 
-    return (
-        <BooksContext.Provider value={valueToShare}>
-            {children}
-        </BooksContext.Provider>
-    );
-}
+    return <BooksContext.Provider value={valueToShare}>{children}</BooksContext.Provider>;
+};
 
 export { Provider };
 export default BooksContext;
